0a029a7a40189d1804a2573d36826486d2121a0f
maysam <maysam82.dev@gmail.com>
2020-08-25 18:10:49 +1000

    Updated logics for Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { routes } from '../../routes';
 
 const Login = () => {
-    return <div>Login</div>;
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+
+    const handleChange = ({ target: { name, value } }) => {
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Sign Into Your Account
+            </p>
+            <form className="form" onSubmit={(event) => handleSubmit(event)}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={(event) => handleChange(event)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={(event) => handleChange(event)}
+                    />
+                </div>
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
+                />
+                <Link to={routes.HOME} className="btn btn-light">
+                    Cancel
+                </Link>
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to={routes.REGISTER}>Sign Up</Link>
+            </p>
+        </Fragment>
+    );
 };
 
 export default Login;
